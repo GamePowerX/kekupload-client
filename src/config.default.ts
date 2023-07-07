3655dec9c843cf6c3cf09ab15275347275e43a31
CraftingDragon007 <craftingdragon007@outlook.com>
2023-07-07 11:44:35 +0200

    Added config option and actual implementation

--- src/config.default.ts
+++ src/config.default.ts
@@ -4,6 +4,7 @@ export const title = "KekUpload";
 export const description = "Upload everything";
 export const base_download = "http://localhost:6942/";
 export const api = new KekUploadAPI("http://localhost:6942/api/");
+export const upload_with_filename = true;
 export const uploader = new FileUploaderQueued({
 	read_size: 1024 * 1024 * 32,
 	chunk_size: 1024 * 1024 * 2,
